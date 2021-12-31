963bda25b6c63261d6fe528300f0216645605540
Alexandre <hallyssonrdev@gmail.com>
2021-12-31 11:05:16 -0300

    all content

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<title></title>
+</head>
+<body>P
+	<section>
+		<div id="particles-js">
+			<div class="hero-container">
+				<div class="environment"></div>
+				<h2 class="hero glitch layers" data-text="Wizzy">
+					<span>Wizzy</span>
+				</h2>
+			</div>
+		</div>
+	</section>
+	<script src="https://cdn.jsdelivr.net/particles.js/2.0.0/particles.min.js">
+		
+	</script>
+	<script type="text/javascript" src="js/script.js"></script>
+</body>
+</html>
